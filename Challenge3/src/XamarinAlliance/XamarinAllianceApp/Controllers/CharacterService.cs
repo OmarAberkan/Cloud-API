988901c3388a8cd92e9d80e67fce494a014cfcd8
OmarAberkan <s088342@ap.be>
2017-03-23 23:38:35 +0100

    Code op veel manieren toegepast werkt niet!

--- Challenge3/src/XamarinAlliance/XamarinAllianceApp/Controllers/CharacterService.cs
+++ Challenge3/src/XamarinAlliance/XamarinAllianceApp/Controllers/CharacterService.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -12,13 +13,19 @@ namespace XamarinAllianceApp.Controllers
 {
     public class CharacterService
     {
+
         /// <summary>
         /// Get the list of characters
         /// </summary>
         /// <returns>ObservableCollection of Character objects</returns>
         public async Task<ObservableCollection<Character>> GetCharactersAsync()
         {
-            var characters = await ReadCharactersFromFile();
+            string mobileServiceClientUrl = "http://xamarinalliancebackend.azurewebsites.net";
+            MobileServiceClient Client = new MobileServiceClient(mobileServiceClientUrl);
+            IMobileServiceTable<Character> CharacterTable = Client.GetTable<Character>();
+         
+            var characters = await CharacterTable.ToListAsync();
+       
             return new ObservableCollection<Character>(characters);
         }
 
